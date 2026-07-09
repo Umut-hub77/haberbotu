e387a5e28a3f30caa65be484d92e38c6d67395f8
Umut Emre ÇELEBİ <143948582+Umut-hub77@users.noreply.github.com>
2026-07-09 16:10:22 +0300

    Update haber_botu.py

--- haber_botu.py
+++ haber_botu.py
@@ -7,7 +7,7 @@ from email.mime.text import MIMEText
 def kategorili_haberleri_cek():
     # İngilizce ve Türkçe kaynakların karmaşık (mixed) listesi
     kategori_kaynaklari = {
-    "Kurumsal Güvenlik ve Tehdit İstihbaratı": "https://www.bleepingcomputer.com/feed/",
+    "Kurumsal Güvenlik ve Tehdit İstihbaratı:" "https://www.bleepingcomputer.com/feed/",
     "Global Teknoloji": "https://shiftdelete.net/feed",
     "Yapay Zeka:" "https://techcrunch.com/category/artificial-intelligence/feed/"
     }
